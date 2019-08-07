234934f0b3e2413b70cbd2fe4770ef0de6c14c2b
unknown <Joe Liu>
2019-08-06 19:00:45 -0700

    collecting orders start

--- backOrder.js
+++ backOrder.js
@@ -36,6 +36,8 @@ const self = {
     var button = await self.page.$("div.clearfix");
     await button.click();
 
+    await displayHundred();
+
     const searchInputSelector = "#dataTables-backorders2_filter > label > input"
     await self.page.waitForSelector(searchInputSelector);
     const searchInput = await self.page.$(searchInputSelector);
@@ -47,10 +49,50 @@ const self = {
       self.page.type(searchInputSelector,item.STYLENUM);
 
       //do something...
-      await sleep(3000);
+      await sleep(1000);
+      var orders = await collectOrders();
+      
     }
 
   }
+}
+
+
+const displayHundred = async() => {
+  var select =
+    "#dataTables-backorders2_length > label > select";
+  await self.page.waitForSelector(select);
+  await self.page.select(select, "100");
+  await sleep (100);
+  console.log("Display 100 orders");
+};
+
+const collectOrders = async () => {
+  var orders = [];
+  const table = await self.page.$$("#dataTables-backorders2 > tbody > tr");
+  for ( row of table) {
+    let order = {};
+
+    var records = await row.$$("#dataTables-backorders2 > tbody > tr > td");
+ 
+    let vender = records[1];
+    let originalNum = records[2];
+    let color = records[4];
+    let qty = records[5];
+
+    let venderText = await self.page.evaluate(vender=> vender.innerText, vender);
+    let originalNumText = await self.page.evaluate(originalNum=> originalNum.innerText, originalNum);
+    let colorText = await self.page.evaluate(color=> color.innerText, color);
+    let qtyText = await self.page.evaluate(qty=> qty.innerText, qty);
+
+    order.vender = venderText;
+    order.originalNum = originalNumText;
+    order.color = colorText;
+    order.qty = qtyText;
+    orders.push(order);
+  }
+  console.log(orders);
+  return orders;
 }
 
 module.exports = self;
