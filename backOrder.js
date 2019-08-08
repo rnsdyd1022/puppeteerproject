8a8b366dd692cb4d03128e743898550eb9cd0ebd
unknown <Joe Liu>
2019-08-07 19:28:02 -0700

    continue doing modifyOrder

--- backOrder.js
+++ backOrder.js
@@ -51,6 +51,7 @@ const self = {
       //do something...
       await sleep(1000);
       var orders = await collectOrders();
+      var modifiedOrders = await modifyStyNumOfOrders(orders);
       
     }
 
@@ -74,19 +75,27 @@ const collectOrders = async () => {
     let order = {};
 
     var records = await row.$$("#dataTables-backorders2 > tbody > tr > td");
- 
+    if(records.length === 1) {
+      let msg = records[0];
+      let msgText = await self.page.evaluate(msg => msg.innerText, msg) ;
+       console.log(msgText);
+      continue;
+    }
     let vender = records[1];
     let originalNum = records[2];
+    let styNum = records[3];
     let color = records[4];
     let qty = records[5];
 
     let venderText = await self.page.evaluate(vender=> vender.innerText, vender);
     let originalNumText = await self.page.evaluate(originalNum=> originalNum.innerText, originalNum);
+    let styNumText = await self.page.evaluate(styNum=> styNum.innerText, styNum);
     let colorText = await self.page.evaluate(color=> color.innerText, color);
     let qtyText = await self.page.evaluate(qty=> qty.innerText, qty);
 
     order.vender = venderText;
     order.originalNum = originalNumText;
+    order.styNum = styNumText;
     order.color = colorText;
     order.qty = qtyText;
     orders.push(order);
@@ -95,4 +104,49 @@ const collectOrders = async () => {
   return orders;
 }
 
+const modifyStyNumOfOrders = async (orders) => {
+
+  var modifiedOrders = orders.map((order)=> {
+
+    //original# = "" -- LASHOWROOM
+    if(order.originalNum === '') {
+      console.log("LA SHOWROOM")
+      let st = order.styNum;
+      var deletTargets = ["PLUS", "PLU","PL"];
+      order.SIZE = "R";
+      for (target of deletTargets) {
+        if( st.indexOf(target) > 0 ) {
+          order.SIZE = "P"
+          st = st.slice(0,st.indexOf(target)).trim();
+        }
+      }
+      var isFirstDash = 0
+      for( let i = 0; i < st.length; i++ ) {
+        
+        if(st[i] ==='-') {
+          if(isFirstDash === 0) {
+            var subSt = st.subStr(0,i)
+          }
+        }
+      }
+
+
+    } else {
+      console.log("FASHION GO")
+
+     let st = order.originalNum;
+     //FashionGO
+      if ( st.indexOf("PLUS") < 0 ) {
+
+         order.SIZE = "R";
+
+      } else if (st.indexOf("PLUS") > 0 ){
+
+         order.SIZE = "P";
+         order.originalNum = st.slice(0,styleNum.indexOf("PLUS")).trim();
+
+      }
+    }
+  })
+}
 module.exports = self;
